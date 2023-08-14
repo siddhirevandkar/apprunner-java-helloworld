94679ac3657dd4b5173bd17c66adba5cbf1d4187
mmasaaud <mmasaaud@amazon.com>
2023-08-14 13:31:57 -0500

    Add AWS SDK Functions1

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,10 +1,33 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloWorld {
+	
+    Region region = Region.US_EAST_1;
+    String bucketName = "apprunner-test-app1-java";
+    
+    ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
+    S3Client s3 = S3Client.builder()
+        .region(region)
+        .credentialsProvider(credentialsProvider)
+        .build();
+
 
     @GetMapping("/")
     public String index() {
@@ -13,8 +36,23 @@ public class HelloWorld {
     }
     
     @GetMapping("/s3")
-    public String GetObject(){
-    	return "Hello S3";
+    public List<String> GetObject(){
+//    	return "Hello S3";
+    	ListObjectsRequest listObjects = ListObjectsRequest
+                .builder()
+                .bucket(bucketName)
+                .build();
+
+            ListObjectsResponse res = s3.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            List<String> objectsStr = new ArrayList<String>();
+            for (S3Object myValue : objects) {
+            	objectsStr.add(myValue.key());
+//                System.out.print("\n The name of the key is " + myValue.key());
+//                System.out.print("\n The object is " + calKb(myValue.size()) + " KBs");
+//                System.out.print("\n The owner is " + myValue.owner());
+            }
+            return objectsStr;
     }
     
     @GetMapping("/ddb")
